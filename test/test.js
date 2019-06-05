eb787102aa87e78567b5cc130cfdf45cd61130ad
Lalo <lalo@rem-lt-190418a.ad.unosquare.com>
2019-06-05 13:57:00 -0500

    works.

---
+++ test/test.js
@@ -0,0 +1,29 @@
+const chai = require("chai");
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+describe('test mocha and chai themselves are woring :)', function() {
+    var foo = true;
+
+    it('should use assert and pass', function() {
+        assert.equal(foo, true);
+    });
+
+    it('should use expect and pass', function() {
+        expect(foo).to.equal(true);
+    });
+
+    it('should use should and pass', function() {
+        foo.should.equal(true);
+    });
+});
+
+// var assert = require('assert');
+// describe('test mocha and chai themselves are woring :)', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+// });
