a441dbe248d8f534583d0e374efc6a65e5a9e8a4
Lalo <lalo@rem-lt-190418a.ad.unosquare.com>
2019-06-11 18:51:55 -0500

    issue#11 add prettier and set eslint to work with it.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import { Card } from './card';
 import { ANIMALS, PRESENTS, COLORS } from './constants';
 import { Deck } from './deck';
 
-export {
-    Card,
-    ANIMALS,
-    PRESENTS,
-    COLORS,
-    Deck
-};
+export { Card, ANIMALS, PRESENTS, COLORS, Deck };
