3ae8ffd1ebb93c8bf6fb4edf844b05339292d9a5
Lalo <lalo@rem-lt-190418a.ad.unosquare.com>
2019-06-21 10:12:40 -0500

    issue#16 WIP
    Added functionality to deck class
    Test new functionality for deck

--- src/deck.js
+++ src/deck.js
@@ -3,13 +3,7 @@ import { InvalidCardDataError } from './errors/invalid_card_data_error';
 
 export class Deck {
     constructor(cards = null) {
-        if (!cards) {
-            this.cards = DEFAULT_CARDS_DATA;
-        } else if (!Deck.validateCardsData(cards)) {
-            throw new InvalidCardDataError(JSON.stringify(cards));
-        } else {
-            this.cards = cards;
-        }
+        this.reset(cards);
     }
 
     static validateCardsData(cardsData) {
@@ -17,5 +11,37 @@ export class Deck {
             return false;
         }
         return true;
+    }
+
+    shuffle() {
+        const halfCardsLength = Math.ceil(this.cards.length / 2);
+        const firstHalfCards = this.cards.slice(0, halfCardsLength);
+        const secondHalfCards = this.cards.slice(halfCardsLength);
+        let shuffledCards = [];
+
+        while(firstHalfCards.length || secondHalfCards.length) {
+            if (secondHalfCards.length) {
+                shuffledCards.push(secondHalfCards.shift());
+            }
+            if (firstHalfCards.length) {
+                shuffledCards.push(firstHalfCards.shift());
+            }
+        }
+
+        this.cards = shuffledCards;
+    }
+
+    getTopCard() {
+        return this.cards.pop();
+    }
+
+    reset(cards) {
+        if (!cards) {
+            this.cards = DEFAULT_CARDS_DATA;
+        } else if (!Deck.validateCardsData(cards)) {
+            throw new InvalidCardDataError(JSON.stringify(cards));
+        } else {
+            this.cards = cards;
+        }
     }
 }
--- test/test_deck.js
+++ test/test_deck.js
@@ -5,6 +5,19 @@ import { Card } from '../src/card';
 import { Deck } from '../src/deck';
 import { COLORS, ANIMALS, PRESENTS } from '../src/constants';
 
+function testCardsAreDifferent(cardA, cardB) {
+    return (
+        (cardA.animal !== cardB.animal) ||
+        (cardA.present !== cardB.present) ||
+        (cardA.hatColor !== cardB.hatColor) ||
+        (cardA.eyeGlassesColor !== cardB.eyeGlassesColor) ||
+        (cardA.scarfColor !== cardB.scarfColor) ||
+        (cardA.topColor !== cardB.topColor) ||
+        (cardA.bottomColor !== cardB.bottomColor) ||
+        (cardA.shoesColor !== cardB.shoesColor)
+    );
+}
+
 describe('Test Deck class', function() {
     it('should create Deck with default card data', function() {
         let deck = new Deck();
@@ -23,6 +36,33 @@ describe('Test Deck class', function() {
                 return new Deck(cards);
             }
             expect(() => createDeck([])).to.throw(Error, 'Invalid cards data []');
+        });
+    });
+
+    describe('Test suffle', function() {
+        it('should arrenge cards in other order', function() {
+            const deck = new Deck();
+            const originalCardSet = [...deck.cards];
+            deck.shuffle();
+            const shuffledCardSet = [...deck.cards];
+            console.log("original", originalCardSet)
+            console.log("shuffled", shuffledCardSet)
+
+            assert.equal(originalCardSet.length, shuffledCardSet.length);
+
+            assert.isTrue(testCardsAreDifferent(originalCardSet[0], shuffledCardSet[0]));
+        });
+    });
+
+    describe('Test getTopCard', function() {
+        it('should get a different card from top of the deck every time', function() {
+            const deck = new Deck();
+            const originalDeckLength = deck.cards.length;
+            const firstCardFromTop = deck.getTopCard();
+            expect(deck.cards.length).to.equal(originalDeckLength - 1);
+            const secondCardFromTop = deck.getTopCard();
+            expect(deck.cards.length).to.equal(originalDeckLength - 2);
+            assert.isTrue(testCardsAreDifferent(firstCardFromTop, secondCardFromTop));
         });
     });
 });
